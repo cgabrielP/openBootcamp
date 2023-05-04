e9590e4a8bdc0496a67cc80731e658cd0e5cc010
Carlos Peña <cgabriel2303@gmail.com>
2023-05-04 12:17:36 -0400

    Update functions

--- año_bisiesto.py
+++ año_bisiesto.py
@@ -4,7 +4,35 @@ def check_year(year):
             if year%400==0:
                 return True
             return False
-        return True         
+        return True
+    else: 
+        return False         
+
+def daysInMonth( year, month):
+    leap=check_year(year)
+    if month in [4, 6, 9, 11]:
+        days=30
+        return days
+    elif month==2:
+        if leap:
+            days=29
+            return days
+        days=28
+        return days
+    else:
+        days=31
+        return days
+def dayOfYear(year, month, day):
+    days = day
+    for m in range(1, month):
+        days += daysInMonth(year, m)
+    return days
+        
+
 
 tu_año=int(input("ingresa un año "))
+mes=int(input("ingresa un mes "))
+dia=int(input("ingresa un dia "))
 print(check_year(tu_año))
+print(daysInMonth(tu_año,mes))
+print(dayOfYear(tu_año,mes,dia))
